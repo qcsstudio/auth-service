c9ab467798e5dd9b65b11a67c7f156af0ef285e6
varunVerse-creater <varunvermadev0@gmail.com>
2026-01-19 11:33:15 +0530

    added dotenv on seding

--- src/scripts/seed-superadmin.js
+++ src/scripts/seed-superadmin.js
@@ -1,24 +1,30 @@
+require("dotenv").config();   // 👈 ADD THIS LINE AT TOP
+
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const SuperAdmin = require("../modules/superadmin/superadmin.model");
 
-
 (async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
 
-  const exists = await SuperAdmin.findOne();
-  if (exists) {
-    console.log("Super admin already exists");
-    process.exit();
+    const exists = await SuperAdmin.findOne();
+    if (exists) {
+      console.log("Super admin already exists");
+      process.exit(0);
+    }
+
+    const hashed = await bcrypt.hash("Admin@123", 10);
+
+    await SuperAdmin.create({
+      email: "superadmin@qcs.com",
+      password: hashed
+    });
+
+    console.log("Super admin created");
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
   }
-
-  const hashed = await bcrypt.hash("Admin@123", 10);
-
-  await SuperAdmin.create({
-    email: "superadmin@qcs.com",
-    password: hashed
-  });
-
-  console.log("Super admin created");
-  process.exit();
 })();
