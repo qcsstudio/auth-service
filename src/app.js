11e5a52c753a2fa3d602b897b19b18de5beccee2
varunVerse-creater <varunvermadev0@gmail.com>
2026-01-19 15:52:01 +0530

    added dotenv on seding

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+/* CORS CONFIG */
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+/* BODY PARSER */
 app.use(express.json());
 
+/* ROUTES */
 app.use("/auth/superadmin", require("./modules/superadmin/superadmin.routes"));
-
 app.use("/companies", require("./modules/companies/company.routes"));
 app.use("/users", require("./modules/users/user.routes"));
-
 
 module.exports = app;
