d5637016f7d511dbbed16f289eab1a04f4f3431d
varunVerse-creater <varunvermadev0@gmail.com>
2026-01-20 17:50:29 +0530

    added invite module to auth service

--- src/app.js
+++ src/app.js
@@ -4,18 +4,22 @@ const cors = require("cors");
 const app = express();
 
 /* CORS CONFIG */
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://qcshrms.vercel.app",
+];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like Postman)
+    origin: (origin, callback) => {
+      // allow Postman / server-to-server calls
       if (!origin) return callback(null, true);
 
       if (allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error("Not allowed by CORS"));
+        callback(new Error("CORS not allowed"));
       }
     },
     methods: ["GET", "POST", "PUT", "DELETE"],
