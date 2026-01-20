0d90aa52bb954e57cd055d3dd16cca0e4f561c63
varunVerse-creater <varunvermadev0@gmail.com>
2026-01-20 17:01:14 +0530

    added invite module to auth service

--- src/app.js
+++ src/app.js
@@ -4,9 +4,20 @@ const cors = require("cors");
 const app = express();
 
 /* CORS CONFIG */
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -18,7 +29,6 @@ app.use(express.json());
 /* ROUTES */
 app.use("/auth/superadmin", require("./modules/superadmin/superadmin.routes"));
 app.use("/invites", require("./modules/invites/invite.routes"));
-
 app.use("/companies", require("./modules/companies/company.routes"));
 app.use("/users", require("./modules/users/user.routes"));
 
