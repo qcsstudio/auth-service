46621d810a7f2b698fd951e686813b627e762408
varunVerse-creater <varunvermadev0@gmail.com>
2026-01-22 16:57:27 +0530

    added superadmindashboard api

--- src/modules/invites/invite.routes.js
+++ src/modules/invites/invite.routes.js
@@ -3,6 +3,7 @@ const auth = require("../../middlewares/auth.middleware");
 const controller = require("./invite.controller");
 
 router.post("/send-setup-link", auth, controller.sendSetupLink);
-router.get("/validate", controller.validateInvite);
+router.get("/validate-invite", controller.validateInvite);
+
 
 module.exports = router;
