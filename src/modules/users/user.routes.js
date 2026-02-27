11ac2aae4ef7dc5508fb8f303f7870faa01dc872
varunVerse-creater <varunvermadev0@gmail.com>
2026-02-27 18:07:40 +0530

    remove duplicate change pass api

--- src/modules/users/user.routes.js
+++ src/modules/users/user.routes.js
@@ -3,7 +3,6 @@ const controller = require("./user.controller");
 const tenantMiddleware = require("../../middlewares/tenant.middleware");
 // company admin login
 router.post("/company/login", controller.login);
-router.post("/change-password", controller.changePassword);
 router.post("/internal-create", controller.createInternalUser);
 router.post("/send-otp", controller.sendOTP);
 router.post("/verify-otp", controller.verifyOTP);
