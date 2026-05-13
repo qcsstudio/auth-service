a1d8ad34a684b3a04b899978b3f8b74403631e52
parvinder7528 <singhparvinder2831@gmail.com>
2026-05-13 10:57:14 +0530

    here chnages done only issue solve

--- src/modules/superadmin/superadmin.routes.js
+++ src/modules/superadmin/superadmin.routes.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const controller = require("./superadmin.controller"); // your existing controller
 const tenantMiddleware = require("../../middlewares/tenant.middleware"); // detect subdomain
-const authMiddleware = require("../../middlewares/auth.middleware");     
-
+     
+const authMiddleware = require("../../middlewares/auth.middleware"); // protect routes
 /* ===================== LOGIN ===================== */
 // Single login endpoint for both SuperAdmin and Company Admin
 router.post("/login", tenantMiddleware, controller.login);
